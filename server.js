8f9833248fc5423e02908ea8fef08665627bff82
Sheldon Perkins <csperkins18@gmail.com>
2019-12-26 23:23:57 -0500

    app listen

--- server.js
+++ server.js
@@ -30,3 +30,8 @@ app.use(express.static("public"));
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
 mongoose.connect(MONGODB_URI);
+
+
+app.listen(PORT, function() {
+    console.log("App running on PORT " + PORT);
+  });
