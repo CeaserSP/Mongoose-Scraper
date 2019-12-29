f21f4ae0378ec7a8013c14af809c98f665aee427
Sheldon Perkins <csperkins18@gmail.com>
2019-12-28 19:37:10 -0500

    not using controllers just going with html and js

--- controllers/scrap_controller.js
+++
@@ -1,7 +0,0 @@
-var express = require("express");
-var router = express.Router();
-var request = require("request");
-var cheerio = require("cheerio");
-var mongoose = require("mongoose");
-
-// TO DO: Set up controllers file
--- server.js
+++ server.js
@@ -31,6 +31,11 @@ var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines
 
 mongoose.connect(MONGODB_URI);
 
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 app.listen(PORT, function() {
     console.log("App running on PORT " + PORT);
