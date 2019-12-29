1f4bc0108d2321802ffe50120b5d34df67dc40da
Sheldon Perkins <csperkins18@gmail.com>
2019-12-28 20:39:32 -0500

    mongoose promise, Not using handlebars

--- server.js
+++ server.js
@@ -28,15 +28,6 @@ app.use(express.static("public"));
 
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
-
+mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI);
 
-// Set Handlebars.
-var exphbs = require("express-handlebars");
-
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
-
-app.listen(PORT, function() {
-    console.log("App running on PORT " + PORT);
-  });
